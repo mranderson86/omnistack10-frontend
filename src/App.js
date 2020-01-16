bb83f11d21113d7e7f41aa7f206eeff0583117e1
Anderson <mr.anderson.brito@gmail.com>
2020-01-15 22:59:17 -0300

    Criação e estilização formulário de cadastro do developer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
 import React from 'react';
 
-import Header from './Header';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+
+import avatar from './logo192.png';
 
 function App() {
   return (
-    <Header></Header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+
+          <div class="input-block">
+            <label htmlFor="username_github">Usuário do Github</label>
+            <input name="github_username" id="username_github" required></input>
+          </div>
+          
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+          
+          <div className="input-group">
+
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+
+          </div>
+
+          <button type='submit'>Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src={avatar} alt="" />
+              <div className="user-info">
+                <strong>Anderson Gomes</strong>
+                <span>ReactJS, React Native , Node.JS</span>
+              </div>
+            </header>
+          </li>
+        </ul>
+      </main>
+
+    </div>
   );
 }
 
