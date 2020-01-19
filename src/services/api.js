7c66b5e555218148cd510f6e140abaf8cd54e2d1
Anderson <mr.anderson.brito@gmail.com>
2020-01-19 14:16:30 -0300

    Configurado variável de ambiente para deploy no heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    //baseURL: 'http://localhost:3333'
+    baseURL: 'https://arcane-depths-49398.herokuapp.com'
 });
 
 export default api;
