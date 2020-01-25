164b820a70d22a09a9314d3104de7cc06a0692fd
mranderson86 <mr.anderson.brito@gmail.com>
2020-01-25 16:27:03 -0300

    setado endereço do backend na nuvem

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.106:3333'
-    //baseURL: 'https://arcane-depths-49398.herokuapp.com'
+    //baseURL: 'http://192.168.0.106:3333'
+    baseURL: 'https://arcane-depths-49398.herokuapp.com'
 });
 
 export default api;
